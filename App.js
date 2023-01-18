929244882d3e4034f14be371e47652bdcb348aef
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-18 14:41:51 +0530

    project folder and tailwind css setup

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-700">
+        Open up App.js to start working on your app!
+      </Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
