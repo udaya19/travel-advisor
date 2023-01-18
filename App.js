630da6ee300f13887e90e60d7f81a231d85e1c07
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-18 15:14:20 +0530

    navigation

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-700">
-        Open up App.js to start working on your app!
-      </Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
